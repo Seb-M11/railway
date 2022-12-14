ee4a385ba608b17a86740bababb78d3330724c0f
Seb-M11 <61061771+Seb-M11@users.noreply.github.com>
2022-12-14 22:17:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
