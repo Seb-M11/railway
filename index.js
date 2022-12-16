50d635845e8429fc1de885b732bc1f11f577d690
Seb-M11 <sebastian.12345.ns.67@gmail.com>
2022-12-16 13:11:56 +0100

    eluwa

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const port = 3000
 var mysql = require('mysql');
 
-/*var host = process.env.DB_HOST || "localhost"
+var host = process.env.DB_HOST || "localhost"
 var user = process.env.DB_USER || "root"
 var password = process.env.DB_PASSWORD || ""
 var db = process.env.DB_NAME || "elo"
 var port1 = process.env.DB_PORT || 3306
+
 
 var con = mysql.createConnection({
   host: host,
@@ -26,10 +27,10 @@ var con = mysql.createConnection({
   });*/
 
 
-  /*con.connect(function(err) {
+  con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-  });*/
+  });
 
 
 app.get('/', (req, res) => {
