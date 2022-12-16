328e9e29539dd8f37c5ab656d810ec8e66d5b0fc
Seb-M11 <sebastian.12345.ns.67@gmail.com>
2022-12-16 11:52:21 +0100

    elo2

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var con = mysql.createConnection({
   host: process.env.MYSQLHOST || "localhost",
   user: process.env.MYSQLUSER || "root",
   password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "elo"
+  database: process.env.MYSQLDATABASE || "elo",
+  port: process.env.MYSQLPORT || 3306
 });
 
 
